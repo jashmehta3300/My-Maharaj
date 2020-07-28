45bbb69a29fb86208162b9a4bcc810baed81f742
Bhavyalakhani <bhavyalakhani89@gmail.com>
2020-07-28 15:46:00 +0530

    solved some issues

--- frontend/MaharajAdmin/src/screen/AdminLogin.js
+++ frontend/MaharajAdmin/src/screen/AdminLogin.js
@@ -1,76 +1,137 @@
-
-
-import React, { Component } from "react";
-import { 
-    View,
-    Text,
-    StyleSheet,
-    TextInput,
-    TouchableOpacity
-} from "react-native";
+import React from 'react';
+import { Text, StyleSheet, ImageBackground , Image, View , TextInput , TouchableOpacity,LayoutAnimation,UIManager, Alert} from 'react-native';
+import AsyncStorage from "@react-native-community/async-storage"
 import * as Animatable from 'react-native-animatable'
 
-class AdminLogin extends Component {
+if (Platform.OS === 'android') {
+    if (UIManager.setLayoutAnimationEnabledExperimental) {
+      UIManager.setLayoutAnimationEnabledExperimental(true);
+    }
+  }
+
+export default class LoginScreen extends React.Component{
     constructor(props){
-        super(props);
-        this.state={
-            OTP:true
+        super(props)
+        this.state = {
+            OTP : true,
+            tokens:"",
+            mobile:"",
+            OTP_value:""
+
+
         }
     }
-    render() {
-        return (
+    sendotp =async ()=>{                                                                //fetching the send sms api and handling with errors 
+        if(this.state.mobile){
+            LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut);
+            this.setState({OTP:false})
+               await fetch('http://localhost:5000/api/v1/maharajAuth/sms',{
+               method:'POST',
+                body:JSON.stringify({
+                    mobile:this.state.mobile,                 
+                }),
+                redirect:'follow',
+                headers:{
+                    "Content-Type":"application/json"
+                }
+            })
+            .catch((error) =>{
+                Alert.alert(error)
+            } ) 
+        }
+        else{
+            Alert.alert("Please enter your mobile no")
+        }
+
+    }
+    loginotp= async () =>{                         //verifying your otp and handling errors 
+        if(this.state.OTP_value){
+         await fetch("http://localhost:5000/api/v1/maharajAuth/login",{
+                method:"POST",
+                body:JSON.stringify({
+                    mobile:this.state.mobile,
+                    token:this.state.OTP_value,
+                }),
+                headers:{
+                    "Content-Type":"application/json"
+                }
+            })
+            .then((response) =>response.json())
+            .then((data) =>{
+                console.warn(data)
+                if(data.success){
+                    this.setState({tokens:data.token})
+                    AsyncStorage.setItem('Admintoken',this.state.tokens)
+                    console.warn(this.state.tokens)
+                    this.props.navigation.navigate('MainAdmin')
+                }
+                else{
+                    Alert.alert("Login failed.Enter the valid OTP")
+                }
+            })
+            .catch((error) =>{
+            
+                Alert.alert(error)
+            });
+
+        }
+        else{
+            Alert.alert("Please enter the OTP")
+
+        }
+    }
+ 
+render(){
+    return(
             <View style = {style.container}>
-            <Text style = {{fontSize:40 , alignItems:'center' , alignSelf:'center' , fontWeight:'bold' , marginTop:100 , marginBottom:100}}>Admin Login</Text>
-            <Animatable.View
-                animation='fadeInUpBig'>          
-            <View style = {{flexDirection:'row' ,  borderWidth:1 , marginLeft:50, marginRight:50 , borderColor:'grey' , borderRadius:10}}>
-            <Text style={style.text}>+91</Text>
-            <TextInput
-              keyboardType = {'numeric'}
-              placeholder = 'Phone Number'
-              onChangeText={(text) => this.setState({mobile:text})}
-              style={style.textinput}
+                <Text style = {{fontSize:40 , alignItems:'center' , alignSelf:'center' , fontWeight:'bold' , marginTop:100 , marginBottom:100}}>User Login</Text>
+                <Animatable.View
+                animation='fadeInUpBig'>
+                <View style = {{flexDirection:'row' ,  borderWidth:1 , marginLeft:50, marginRight:50 , borderColor:'grey' , borderRadius:10}}>
+                <Text style={style.text}>+91</Text>
+                <TextInput
+                keyboardType = {'numeric'}
+                placeholder = 'Phone Number'
+                onChangeText={(text) => this.setState({mobile:text})}
+                style={style.textinput}
 
-            >
-            </TextInput>
-            </View>
-        { this.state.OTP ? 
-        <View>
-           <TouchableOpacity style={{alignSelf:'center' , backgroundColor:'#000' , marginTop:30 , borderRadius:10}} onPress = {() => this.setState({OTP:false})}>
-                <Text style = {style.button}>Send OTP</Text>
-            </TouchableOpacity>
-
-        </View>
-            :
+                >
+                </TextInput>
+                </View>
+            { this.state.OTP ? 
             <View>
-            <View style = {{  borderWidth:1 , marginLeft:50, marginRight:50 , borderColor:'grey' , borderRadius:10 , marginTop:40}}>
-            <TextInput
-              keyboardType = {'numeric'}
-              placeholder = 'OTP'
-              onChangeText={(text) => {this.setState({OTP_value:text})}}
-              style={style.textinput}
-            >
-            </TextInput> 
+            <TouchableOpacity style={{alignSelf:'center' , backgroundColor:'#000' , marginTop:30 , borderRadius:10}} onPress = {() => this.sendotp()}>
+                    <Text style = {style.button}>Send OTP</Text>
+                </TouchableOpacity>
             </View>
-            <TouchableOpacity style={{alignSelf:'center' , backgroundColor:'#000' , marginTop:30 , borderRadius:10}} >
-            <Text style = {style.button}>Resend OTP</Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={{alignSelf:'center' , backgroundColor:'#000' , marginTop:30 , borderRadius:10}} onPress = {() => {this.props.navigation.navigate('MainAdmin')}}>
-            <Text style = {style.button}>Confirm OTP</Text>
-            </TouchableOpacity>
+                :
+                <View>
+                <View style = {{  borderWidth:1 , marginLeft:50, marginRight:50 , borderColor:'grey' , borderRadius:10 , marginTop:40}}>
+                <TextInput
+                keyboardType = {'numeric'}
+                placeholder = 'OTP'
+                onChangeText={(text) => {this.setState({OTP_value:text})}}
+                style={style.textinput}
+                >
+                </TextInput> 
+                </View>
+                <TouchableOpacity style={{alignSelf:'center' , backgroundColor:'#000' , marginTop:30 , borderRadius:10}} onPress = {() => {this.sendotp()}}>
+                <Text style = {style.button}>Resend OTP</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={{alignSelf:'center' , backgroundColor:'#000' , marginTop:30 , borderRadius:10}} onPress = {() => {this.loginotp()}}>
+                <Text style = {style.button}>Confirm OTP</Text>
+                </TouchableOpacity>
+                </View>
+            }
+            </Animatable.View>
             </View>
-        }
-        </Animatable.View>
-        </View>
-        );
-    }
+)}
 }
-export default AdminLogin;
-
 const style = StyleSheet.create({
     container: {
-        flex: 1,
+        flex:1,        
     },
+
     text:{
         fontSize:25,
         borderRightWidth:1,
@@ -97,5 +158,3 @@ const style = StyleSheet.create({
         
     } , 
 })
-
-
